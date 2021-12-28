3dc0b909c7068c5aba0efc83850827554f9d404f
ivannikanorov201 <81432550+ivannikanorov201@users.noreply.github.com>
2021-12-28 09:08:31 +0900

    Add files via upload

---
+++ Lub_4/Ex5.java
@@ -0,0 +1,12 @@
+public class Ex5 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw new NullPointerException("ошибка");
+        }
+        catch (NullPointerException e) {
+            System.out.println("1" );
+        }
+        System.out.println("2");
+    }
+}
